f27a220405f3c9a280ef169551714ab6f59c61c8
Kaleb Valenzuela <d.kal3b@gmail.com>
2023-12-13 10:46:12 -0800

    Background work

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Dynamic Background Color Change on Scroll
+document.addEventListener('scroll', function () {
+    var scrollPosition = window.scrollY;
+
+    // Change background color on scroll
+    if (scrollPosition > 50) {
+        document.body.classList.add('nav-scrolled');
+        document.querySelectorAll('.section').forEach(function (section) {
+            section.classList.add('section-scrolled');
+        });
+    } else {
+        document.body.classList.remove('nav-scrolled');
+        document.querySelectorAll('.section').forEach(function (section) {
+            section.classList.remove('section-scrolled');
+        });
+    }
+});
+
 // Smooth Scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
